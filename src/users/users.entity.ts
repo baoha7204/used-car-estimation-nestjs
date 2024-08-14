388f9fcd3415a625ad6110755c3f99d986a44ca6
baoha7204 <baoha3604@gmail.com>
2024-08-14 11:23:40 +0700

    [BaoHMQ] Setup SQLite, basic CRUD User

---
+++ src/users/users.entity.ts
@@ -0,0 +1,35 @@
+import {
+  AfterInsert,
+  AfterUpdate,
+  BeforeRemove,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @BeforeRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
